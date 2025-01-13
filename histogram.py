b36dd89a1c9509a42c709d1ee426ab602b3a912e
Pulcherie Mbaye <pulcheriembaye11@gmail.com>
2025-01-13 01:32:31 -0500

    Update histogram.py

--- histogram.py
+++ histogram.py
@@ -1,15 +1,3 @@
-# ECOR 1042 Lab 6 - Template submission for histogram
-
-# Remember to include docstring and type annotations for your functions
-
-# Update "" with your name (e.g., Cristina Ruiz Martin)
-__author__ = "Alex Rusu"
-
-# Update "" with your student number (e.g., 100100100)
-__student_number__ = "101314298"
-
-# Update "" with your team (e.g. T-102, use the notation provided in the example)
-__team__ = "T-113"
 
 
 import matplotlib.pyplot as plt
